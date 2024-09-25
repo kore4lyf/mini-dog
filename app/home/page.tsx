bb375614ef50d615be9c0a611da46e396af5c229
Korede Faleye <akfaleye@gmail.com>
2024-09-25 15:04:51 +0100

    Add tap effects

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+"use client"
+
+import React, { useState, useEffect } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 import DataBoard from '@/components/DataBoard'
 import TapBtn from '@/components/TapBtn'
 import UserLevelInfo from '@/components/UserLevelInfo'
@@ -10,9 +13,44 @@ import Image from 'next/image'
 import NavMenu from '@/components/NavMenu'
 
 const homePage = () => {
-  const points = 324293
   const activeEnergy = 1392
   const levelMaxEnergy = 1500
+
+  const [count, setCount] = useState(324293)
+  const [pointsPerTap, setPointsPerTap] = useState(2)
+  const [points, setPoints] = useState<{ x: number; y: number; id: number; value: number }[]>([])
+  const [currentEnergy, setCurrentEnergy] = useState(1392)
+  const [currentTapValue, setCurrentTapValue] = useState(0)
+
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const { clientX, clientY } = e
+    setPoints((prev) => [
+      ...prev,
+      { x: clientX , y: clientY, id: Date.now(), value: pointsPerTap },
+    ])
+    setCurrentTapValue(pointsPerTap)
+  }
+
+  
+  const IncreaseEnergy = () => {
+    setInterval(() => {
+      setCurrentEnergy((prevValue) => prevValue + 1)
+    }, 1000)
+  }
+  
+  useEffect(() => IncreaseEnergy(), [])
+
+  useEffect(() => {
+    if (currentTapValue > 0) {
+      const interval = setInterval(() => {
+        setCount((prevCount) => prevCount + 1)
+        setCurrentTapValue((prevValue) => prevValue - 1)
+        setCurrentEnergy((prevValue) => prevValue - 1)
+      }, 100)
+
+      return () => clearInterval(interval)
+    }
+  }, [currentTapValue])
 
   return (
     <main className='relative w-[90%] py-4 mx-auto h-screen grid auto-rows-auto gap-4'>
@@ -21,15 +59,15 @@ const homePage = () => {
       <div className='row-span-12 flex flex-col justify-between'>
 
         <div className='mx-auto'>
-          <Points points={points} />
+          <Points points={count} />
         </div>
         
-        <div className='-translate-y-8 mx-auto'>
-          <TapBtn />
+        <div className='relative -translate-y-8 mx-auto'>
+          <TapBtn action={handleClick} />
         </div>
         
         <div className='flex justify-between h-fit'>
-          <TapEnergy activeEnergy={activeEnergy} levelMaxEnergy={levelMaxEnergy}/>
+          <TapEnergy activeEnergy={currentEnergy} levelMaxEnergy={levelMaxEnergy}/>
           <CircleButton path="/settings">
             <Image src={settingsIcon} alt="Settings icon" width="23" />
           </CircleButton>
@@ -37,6 +75,24 @@ const homePage = () => {
       </div>
       <NavMenu />
       <div className='h-20'></div>
+
+      <AnimatePresence>
+        {points.map((point) => (
+          <motion.div
+            key={point.id}
+            initial={{ opacity: 1, scale: 1 }}
+            animate={{ y: -100, opacity: 0, scale: 2 }}
+            transition={{ duration: 1.5, ease: 'easeOut' }}
+            className="absolute selection:bg-transparent"
+            style={{
+              left: point.x - 16,
+              top: point.y - 16,
+            }}
+          >
+            +{pointsPerTap}
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </main>
   )
 }
--- components/TapBtn.tsx
+++ components/TapBtn.tsx
@@ -1,14 +1,17 @@
 'use client'
 
-import React from 'react'
+import React, { MouseEventHandler } from 'react'
 import Image from 'next/image'
 import miniDog from '@/assets/images/mini-dog.svg';
 
-const TapBtn: React.FC = () => {
-  return (
-    <button className='row-span-2 grid place-content-center translate-y-7'>
-        <Image className='scale-125 w-[256px]' src={miniDog} alt='Dogs Image'/>
+interface TapBtnProps {
+  action: MouseEventHandler<HTMLButtonElement>
+}
 
+const TapBtn: React.FC<TapBtnProps> = ({ action }) => {
+  return (
+    <button onClick={action} className='row-span-2 grid place-content-center translate-y-7'>
+      <Image className='scale-125 w-[256px]' src={miniDog} alt='Dogs Image'/>
     </button>
   )
 }
